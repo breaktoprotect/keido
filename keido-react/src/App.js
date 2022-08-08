edc6844f9a07f7ad40eaf3f3c2392751bec8464f
JS @breaktoprotect <breaktoprotect@gmail.com>
2022-08-09 01:23:02 +0800

    Work in progress

--- keido-react/src/App.js
+++ keido-react/src/App.js
@@ -1,16 +1,23 @@
 //import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import DayCalendar from "./components/dayCalendar";
+import { getScheduledItems } from "./mockServices/schedule";
 
-function App() {
-    return (
-        <div className="App Container">
-            <DayCalendar
-                displayStartDate={new Date(2022, 8, 1)}
-                displayEndDate={new Date(2022, 8, 12)}
-            />
-        </div>
-    );
+class App extends Component {
+    state = { scheduledItems: getScheduledItems() };
+
+    render() {
+        return (
+            <div className="App Container">
+                <DayCalendar
+                    scheduledItems={this.state.scheduledItems}
+                    displayStartDate={new Date(2022, 8 - 1, 15)}
+                    displayEndDate={new Date(2022, 8 - 1, 27)}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- keido-react/src/components/dayCalendar.jsx
+++ keido-react/src/components/dayCalendar.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-import { getScheduledItems } from "../mockServices/schedule";
-import { eachDayOfInterval, format } from "date-fns";
-import { getScheduledItems } from "./../mockServices/schedule";
+import { eachDayOfInterval, format, isWithinInterval } from "date-fns";
 
 class DayCalendar extends Component {
-    state = { scheduleItems: getScheduledItems() };
     render() {
-        const { displayStartDate, displayEndDate } = this.props;
+        const { displayStartDate, displayEndDate, scheduledItems } = this.props;
 
         // Get number of days based on range
         const days = eachDayOfInterval({
@@ -14,9 +11,19 @@ class DayCalendar extends Component {
             end: displayEndDate,
         });
 
+        // Map consultants to each project and start and end dates
+        const consultantsTasks = this.getConsultantsTasks(scheduledItems);
+
+        //debug
+        console.log(consultantsTasks);
+
+        //debug 1 consultant
+        const aConsultant = Object.keys(consultantsTasks)[0];
+        const aTasks = consultantsTasks[aConsultant];
+
         return (
             <React.Fragment>
-                <table className="table">
+                <table className="table table-bordered">
                     <thead>
                         <tr>
                             <th scope="col"></th>
@@ -27,14 +34,98 @@ class DayCalendar extends Component {
                             ))}
                         </tr>
                     </thead>
-                    <tbody>{this.populateSchedule()}</tbody>
+                    <tbody>{this.populateRow(aConsultant, aTasks, days)}</tbody>
                 </table>
             </React.Fragment>
         );
     }
 
-    populateSchedule() {
-        getScheduledItems;
+    getConsultantsTasks(scheduledItems) {
+        // Neat list of consultant and their correspond array of {day:project} keypair values
+        var consultantsTasks = {};
+        scheduledItems.map((item) => {
+            item.consultants.map((consultant) => {
+                if (consultantsTasks[consultant] === undefined) {
+                    consultantsTasks[consultant] = [
+                        {
+                            projectID: item.id,
+                            startDate: item.startDate,
+                            endDate: item.endDate,
+                        },
+                    ];
+                } else {
+                    consultantsTasks[consultant] = [
+                        ...consultantsTasks[consultant],
+                        {
+                            projectID: item.id,
+                            startDate: item.startDate,
+                            endDate: item.endDate,
+                        },
+                    ];
+                }
+            });
+        });
+
+        return consultantsTasks;
+    }
+
+    populateSchedule(days, consultantsTasks) {
+        days.map((day) => {
+            Object.keys(consultantsTasks).map((person) => {
+                consultantsTasks[person].map((project) => {
+                    this.addToCal(day, project);
+                });
+            });
+        });
+    }
+
+    populateRow(consultant, tasks, days) {
+        //debug
+        console.log("tasks", tasks);
+
+        //pre-process
+        let columnItems = [];
+        days.map((day) => {
+            tasks.map((task) => {
+                columnItems = [...columnItems, this.addToCal(day, task)];
+            });
+        });
+
+        //debug
+        console.log("columnItems:", columnItems);
+
+        return (
+            <tr key={consultant}>
+                <th>{consultant}</th>
+                {columnItems.map((item) => (
+                    <td>{item}</td>
+                ))}
+            </tr>
+        );
+    }
+
+    addToCal(day, project) {
+        if (
+            isWithinInterval(day, {
+                start: this.convertToDateObject(project.startDate),
+                end: this.convertToDateObject(project.endDate),
+            })
+        ) {
+            console.log(project.startDate, project.endDate, day, true);
+            return project.projectID;
+        } else {
+            console.log(project.startDate, project.endDate, day, false);
+            return null;
+        }
+    }
+
+    convertToDateObject(dateString) {
+        const dayMonthYearArray = dateString.split("-");
+        return new Date(
+            dayMonthYearArray[2],
+            dayMonthYearArray[1] - 1, // Javascript counts month starting from 0
+            dayMonthYearArray[0]
+        );
     }
 }
 
