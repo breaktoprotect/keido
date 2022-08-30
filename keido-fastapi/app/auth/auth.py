cd582cf39870b8ba90a44e3aa0e448fd01e2aa86
JS @breaktoprotect <breaktoprotect@gmail.com>
2022-08-30 21:09:35 +0800

    Keido API

---
+++ keido-fastapi/app/auth/auth.py
@@ -0,0 +1,48 @@
+from hashlib import algorithms_available
+import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from os import environ
+
+class AuthHandler():
+    SECRETS = environ['SECRETS']
+
+    security = HTTPBearer()
+    pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto') # Bcrypt salt is generated https://passlib.readthedocs.io/en/stable/lib/passlib.hash.bcrypt.html
+
+    def get_password_hash(self, password):
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def encode_token(self, user_id):
+        payload = {
+            'expz': (datetime.utcnow() + timedelta(days=0, minutes=5)).timestamp(), #TODO - bug won't expire or always expire
+            'iat': datetime.utcnow().timestamp(), #issued at time (iat)
+            'sub': user_id
+        }
+
+        #debug
+        print("payload -->", payload)
+
+        return jwt.encode(
+            payload,
+            self.SECRETS,
+            algorithm = 'HS256'
+        )
+    
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, self.SECRETS, algorithms=['HS256'])
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail='Token has expired')
+        except jwt.InvalidTokenError as e:
+            raise HTTPException(status_code=401, detail='Invalid token')
+
+    # Dependency injection
+    def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
+        return self.decode_token(auth.credentials)
--- keido-fastapi/main.py
+++ keido-fastapi/main.py
@@ -1,8 +1,65 @@
+from http.client import HTTPException
 import uvicorn
 from fastapi import FastAPI, Body, Depends
+from app.auth.auth import AuthHandler
+from app.model import AuthDetails
 
 app = FastAPI()
 
+auth_handler = AuthHandler()
+users = [] #TODO for DB in future
+
+#* ********************** Public APIs *************************
+#* Misc
+# Example of an unprotected page
+@app.get('/user/version', tags=['Misc'])
+def version():
+    return {}
+# Server heartbeat
 @app.get("/", tags=["Heartbeat"])
 def heartbeat():
     return {"status": "alive!"}
+
+#* Users 
+# Registration
+@app.post('/user/register', tags=['Users'], status_code=201)
+def register(auth_details: AuthDetails):
+    if any(user['username'] == auth_details.username for user in users):
+        #raise HTTPException(status_code=400, detail="Username is already taken")
+
+        #? Obfuscation 
+        return { "status": "success"}
+
+    # Adding new user
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    users.append({
+        'username': auth_details.username,
+        'hashed_password': hashed_password
+    })
+    return { "status": "success"}
+
+# Login
+@app.post('/user/login', tags=['Users'])
+def login(auth_details: AuthDetails):
+    confirmed_user = None
+    for user in users:
+        if user['username'] == auth_details.username:
+            confirmed_user = user
+            break
+    # User don't exist!
+    if confirmed_user == None:
+        return { "status": "login failed"}
+
+    # Password mismatch!
+    if not auth_handler.verify_password(auth_details.password, confirmed_user['hashed_password']):
+        return { "status": "login failed"}
+
+    # Create token
+    token = auth_handler.encode_token(confirmed_user['username'])
+
+    return { 'token': token}
+
+#? ********************* Private APIs *************************
+@app.get('/users/list', tags=['Users'])
+def list_users(username=Depends(auth_handler.auth_wrapper)):
+    return [user['username'] for user in users]
