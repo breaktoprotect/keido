d9385f3ccfeaf7138f69c2dbf6ab6ad164b1bc7b
JS @breaktoprotect <breaktoprotect@gmail.com>
2022-09-01 20:39:36 +0800

    Fixed the jwt exp bug

--- keido-fastapi/app/auth/auth.py
+++ keido-fastapi/app/auth/auth.py
@@ -3,7 +3,7 @@ import jwt
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from os import environ
 
 class AuthHandler():
@@ -20,13 +20,14 @@ class AuthHandler():
 
     def encode_token(self, user_id):
         payload = {
-            'exp': (datetime.utcnow() + timedelta(days=0, minutes=5)).timestamp(), #TODO - bug won't expire or always expire
-            'iat': datetime.utcnow().timestamp(), #issued at time (iat)
+            'exp': (datetime.now(tz=timezone.utc) + timedelta(minutes=1)).timestamp(), #TODO - bug won't expire or always expire
+            'iat': datetime.now(tz=timezone.utc).timestamp(), #issued at time (iat)
             'sub': user_id
         }
 
         #debug
         print("payload -->", payload)
+        
 
         return jwt.encode(
             payload,
@@ -36,7 +37,10 @@ class AuthHandler():
     
     def decode_token(self, token):
         try:
-            payload = jwt.decode(token, self.SECRETS, algorithms=['HS256'])
+            #debug
+            print("utcnow() ->", datetime.utcnow().timestamp())
+
+            payload = jwt.decode(token, self.SECRETS, algorithms=['HS256'])        
             return payload['sub']
         except jwt.ExpiredSignatureError:
             raise HTTPException(status_code=401, detail='Token has expired')
